15dd54d8b8ad1d41154fb453cf8a2dd579f8f46d
DIVAKARPV1111 <divakarpv9025@gmail.com>
2025-04-21 07:13:12 +0430

    initial commit

---
+++ bjvbfw.py
@@ -0,0 +1,63 @@
+class Parrot:
+
+# instance attributes
+ def __init__(self, name, age):
+   self.name = name
+   self.age = age
+
+# instance method
+ def sing(self, song):
+   return '{} sings {}'.format(self.name, song)
+
+ def dance(self):
+   return '{} is now dancing'.format(self.name)
+
+# instantiate the object
+blu = Parrot('Blu', 10)
+# call our instance methods
+print(blu.sing('Happy'))
+print(blu.dance())
+
+for x in range(2, 6):
+ print(x)
+
+ i = 1
+ while i < 6:
+  print(i)
+   if i == 3:
+     break
+   i += 1
+  
+  class Parrot:
+  
+   def fly(self):
+     print('Parrot can fly')
+  
+   def swim(self):
+     print('Parrot can not swim')
+  
+  class Penguin:
+  
+   def fly(self):
+     print('Penguin can not fly')
+  
+   def swim(self):
+     print('Penguin can swim')
+  
+  # common interface
+  def flying_test(bird):
+    bird.fly()
+  
+  #instantiate objects
+  blu = Parrot()
+  peggy = Penguin()
+  
+  # passing the object
+  flying_test(blu)
+  flying_test(peggy)loop([timeout[,)
+                
+Function
+def name(args):
+ pass                Function
+ def name(args):
+  pass
---
+++ div.js
@@ -0,0 +1,10 @@
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+}
+constructor(props) {
+  super(props)
+
+  this.state = {
+     first
+  }
+}
